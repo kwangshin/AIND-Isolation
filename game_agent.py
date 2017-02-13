4486bcb109107655ec295101baa864ff68dd4667
Kwangshin Oh <kwangshin@gmail.com>
2017-02-13 23:13:04 +0900

    Implement the custom_score function with basic open moves.

--- game_agent.py
+++ game_agent.py
@@ -38,9 +38,13 @@ def custom_score(game, player):
         The heuristic value of the current game state to the specified player.
     """
 
-    # TODO: finish this function!
-    raise NotImplementedError
+    if game.is_loser(player):
+        return float("-inf")
 
+    if game.is_winner(player):
+        return float("inf")
+
+    return float(len(game.get_legal_moves(player)))
 
 class CustomPlayer:
     """Game-playing agent that chooses a move using your evaluation function
